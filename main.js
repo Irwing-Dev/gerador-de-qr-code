d9a177115ec052cd5fd5690f204b959978005187
Irwing <irwing.devbr@gmail.com>
2024-08-29 15:02:27 -0300

    Add download function and improve the visual
    This commit adds some changes, such as a button with the function of
    downloading the generated qrcode and some design improvements that I
    thought were a good idea

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-const textQr = document.querySelector("#textQr");
-const qrcode = document.querySelector("#qrcode");
+const qrCodeContainer = document.getElementById('qrcode');
+const generateButton = document.getElementById('generate');
+const downloadButton = document.getElementById('download');
+const textInput = document.getElementById('text');
+const downloadContainer = document.getElementById("download-container");
+const errorsMsg = document.getElementById("error-msg");
 
-document.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        genQRCode();
+let qrCode;
+
+generateButton.addEventListener('click', () => {
+    if (textInput.value == 0) {
+        errorsMsg.innerHTML = "Por favor, digite algo"
+    } else {
+        errorsMsg.innerHTML = ""
+        const text = textInput.value;
+    
+        // Remove o QR code anterior
+        qrCodeContainer.innerHTML = '';
+    
+        // Gera o novo QR code
+        qrCode = new QRCode(qrCodeContainer, {
+            text: text,
+            width: 256,
+            height: 256,
+        });
+
+        qrCodeContainer.style.display = "flex"
+        downloadContainer.style.display = "flex"
     }
 });
 
-function genQRCode() {
-    if (!textQr.value) return;
-    
-    qrcode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${textQr.value}`;
-    qrcode.style.padding = '0.2em'
-}
+downloadButton.addEventListener('click', () => {
+    if (qrCodeContainer.querySelector('img')) {
+        const qrImage = qrCodeContainer.querySelector('img').src;
+        const link = document.createElement('a');
+        link.href = qrImage;
+        link.download = 'qrcode.png';
+        link.click();
+    } else if (qrCodeContainer.querySelector('canvas')) {
+        const qrCanvas = qrCodeContainer.querySelector('canvas');
+        const link = document.createElement('a');
+        link.href = qrCanvas.toDataURL();
+        link.download = 'qrcode.png';
+        link.click();
+    } else {
+        alert('Por favor, gere o QR Code primeiro!');
+    }
+});
